a2cc06a82c97a9866d548175edb6b9673397d688
Aace <aacejovi@gmail.com>
2025-08-27 16:57:13 +0100

    Project page

--- frontend/src/components/ProjectSection.jsx
+++ frontend/src/components/ProjectSection.jsx
@@ -1,7 +1,7 @@
 import instashot from "../assets/project1.png";
 function ProjectSection() {
   return (
-    <div className="mx-auto max-w-[1440px] py-10">
+    <div className="mx-auto max-w-[1440px] bg-black">
       <div className="lg:w-[600px] w-[343px] px-3" id="work">
         <h2 className="lg:text-[76px] text-[43px] bebas-neue-regular text-[#FFFFFF]">
           Featured Projects
@@ -14,9 +14,9 @@ function ProjectSection() {
         </p>
       </div>
 
-      <div className="mt-15">
-        <div className="flex lg:flex-row flex-col items-center gap-[48px]">
-          <div className="w-[80%] h-[343px] lg:w-[600px] lg:h-[600px] bg-[#1A1A1A] flex flex-col justify-center items-start lg:items-center rounded-[12px] relative gap-5 ">
+      <div className="mt-15 ">
+        <div className="flex lg:flex-row flex-col items-center justify-evenly w-full gap-[48px]">
+          <div className="w-[600px] h-[600px] bg-[#1A1A1A] flex flex-col justify-center items-start lg:items-center rounded-[12px] relative gap-5 ">
             <div className="flex items-center justify-center lg:w-[145px] lg:h-[37px] bg-[#0A0A0A] rounded-[100px] relative lg:absolute top-0 left-0 lg:top-10 lg:left-10">
               <p className="text-[#FFFFFF] manrope text-center ">Team work</p>
             </div>
@@ -28,13 +28,13 @@ function ProjectSection() {
             />
           </div>
 
-          <div className="lg:w-[576px] lg:h-[480px] manrope flex flex-col justify-between">
-            <h2 className="text-[#FFFFFF] lg:text-[32px] leading-[110%]">
+          <div className="w-[343px] h-[473px] lg:w-[576px] lg:h-[480px] manrope flex flex-col lg:justify-between  ">
+            <h2 className="text-[#FFFFFF] text-[24px] lg:text-[32px] leading-[110%]">
               Developed an Instagram clone with full functionality, including
               account creation, image uploads, likes, comments, and real-time
               updates.
             </h2>
-            <p className="text-[#C7C7C7] text-[16px] lg:text-[18px] ">
+            <p className="text-[#C7C7C7] text-[16px]  lg:text-[18px]">
               Teamed up with a designer and fellow developers to build a fully
               functional Instagram clone as part of my learning process in
               full-stack development. This project gave me hands on experience
@@ -46,6 +46,15 @@ function ProjectSection() {
             <div>
               <p className="text-[16px] text-[#FFFFFF]">Project Info</p>
               <div className="divider" />
+              <div className="flex justify-between">
+                <p>Year</p>
+                <p className="text-[#C7C7C7]">2025</p>
+              </div>
+              <div className="divider" />
+              <div className="flex justify-between">
+                <p>Roke</p>
+                <p className="text-[#C7C7C7]">Frontend, Backend</p>
+              </div>
             </div>
           </div>
         </div>
