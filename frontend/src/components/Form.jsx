ddb5551a9ad1093c7de3810c307e3f73396be566
Aace <aacejovi@gmail.com>
2025-08-28 12:15:07 +0100

    About page

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -18,16 +18,34 @@ function Form() {
 
   const submitFomr = async (data) => {
     try {
+      const payload = {
+        ...data,
+        time: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      };
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        data,
+        payload,
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       );
-      reset(), toast("Email sent! You'll hear back shortly");
+      reset(),
+        toast("Email sent! You'll hear back shortly", {
+          style: {
+            background: "#266326",
+            color: "white",
+          },
+        });
     } catch (err) {
       const message = err.text || err.message || "Something went wrong";
-      return toast(`FAILED: ${message}`);
+      return toast(`FAILED: ${message}`, {
+        style: {
+          background: "#F87171", // reddish background
+          color: "white",
+        },
+      });
     }
   };
 
